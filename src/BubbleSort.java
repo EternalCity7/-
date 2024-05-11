d783234ccb9b23415a84e4c3b4ad99f20818c836
Icey <643359474@qq.com>
2024-05-11 10:06:06 +0800

    冒泡排序

---
+++ src/BubbleSort.java
@@ -0,0 +1,13 @@
+public class BubbleSort {
+    public static void bSort(int[] numbers){//小->大
+        for (int i = 0; i < numbers.length-1; i++) {
+            for (int j = 0; j < numbers.length-i-1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("排序前的数组：");
+        printArray(arr);
+
+        BubbleSort.bSort(arr);
+
+        System.out.println("排序后的数组：");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+}
