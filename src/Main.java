93c978e6acd0f9886bbf86ef4cbc9faec3b11fc0
Icey <643359474@qq.com>
2024-05-19 17:49:39 +0800

    快排

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         //int[] numbers = {64, 34, 25, 12, 22, 11, 90,75};
-        int[] numbers ={5,4,3,6,3,7,3,9,7};
-
+        //int[] numbers ={5,4,3,6,3,7,3,9,7};
+        int[] numbers={1,9,4,3,5};
         System.out.println("排序前的数组：");
         printArray(numbers);
 
         //BubbleSort.bSort(numbers);
         //SelectionSort.sSort(numbers);
         //InsertSort.iSort(numbers);
-
+        QuickSort.qSort(numbers,0,numbers.length-1);
         System.out.println("排序后的数组：");
-        printArray(CountSort.cSort(numbers));//计数排序
-        //printArray(numbers);
+//        printArray(CountSort.cSort(numbers));//计数排序
+        printArray(numbers);
     }
 
     public static void printArray(int[] arr) {
--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,4 +1,28 @@
 public class QuickSort {
-    public static void qSort(int[] numbers){//小->大
+    public static void qSort(int[] numbers, int left, int right) {//小->大
+        if (right > left) {
+            int i = left, j = right;
+            int base = numbers[left], basePosition = left;
+            while (j>i){
+                while (j > i && numbers[j] >= base) {
+                    j--;
+                }
+                if(j>i){
+                    numbers[basePosition]=numbers[j];
+                    basePosition=j;
+                }
+                while (j > i && numbers[i] <= base) {
+                    i++;
+                }
+                if(j>i){
+                    numbers[basePosition]=numbers[i];
+                    basePosition=i;
+                }else {
+                    numbers[basePosition]=base;
+                }
+            }
+            qSort(numbers,left,basePosition-1);
+            qSort(numbers,basePosition+1,right);
+        }
     }
 }
